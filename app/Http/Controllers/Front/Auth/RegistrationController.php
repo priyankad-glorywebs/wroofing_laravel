00d1e329446d45e4d76f057dfa90ac99ecc3c5be
rupal-glorywebs <rupal@glorywebsdev.com>
2024-02-16 12:46:21 +0530

    update code - 16-feb-2023

--- app/Http/Controllers/Front/Auth/RegistrationController.php
+++ app/Http/Controllers/Front/Auth/RegistrationController.php
@@ -28,65 +28,65 @@ public function registerStepOne(Request $request){
 }
 
 
-public function register(RegisterRequest $request)
-{
+    public function register(RegisterRequest $request)
+    {
 
-    $user                  = new User;
-    $user->name            = $request->name;
-    $user->email           = $request->email;
-    $user->password        = Hash::make($request->password);
-    $user->contact_number  = $request->contact_number;
-    $user->zip_code        = $request->zip_code;
+        $user                  = new User;
+        $user->name            = $request->name;
+        $user->email           = $request->email;
+        $user->password        = Hash::make($request->password);
+        $user->contact_number  = $request->contact_number;
+        $user->zip_code        = $request->zip_code;
 
-    if ($request->areyoua === "customer") {
-        $user->save();
-         $storagePath = 'customer_profile/';
-        if (!File::exists($storagePath)) {
-            File::makeDirectory($storagePath, 0755, true);
-        }
-        if ($request->hasFile('customer_profile')) {
-            $image = $request->file('customer_profile');
-            $imageName = \Str::random(3).time() . '.' . $image->getClientOriginalExtension();
-            Storage::putFileAs($storagePath, $image, $imageName);
-            $user->profile_image = $storagePath . $imageName;
+        if ($request->areyoua === "customer") {
             $user->save();
+            $storagePath = 'customer_profile/';
+            if (!File::exists($storagePath)) {
+                File::makeDirectory($storagePath, 0755, true);
+            }
+            if ($request->hasFile('customer_profile')) {
+                $image = $request->file('customer_profile');
+                $imageName = \Str::random(3).time() . '.' . $image->getClientOriginalExtension();
+                Storage::putFileAs($storagePath, $image, $imageName);
+                $user->profile_image = $storagePath . $imageName;
+                $user->save();
+            }
+            $user->sendEmailVerificationNotification();
+            //\Log::info('Email verification notification sent.');
+
+        } 
+        if ($request->areyoua === "contractor") {
+        $profileImageName = null; 
+
+        if ($request->hasFile('profile_image')) {
+            $profileImage = $request->file('profile_image');
+            $profileImageName = time() . '_' . $profileImage->getClientOriginalName();
+            $profileImage->storeAs('uploads/contractor_profile', $profileImageName);
         }
-        $user->sendEmailVerificationNotification();
-           //\Log::info('Email verification notification sent.');
-
-    } 
-    if ($request->areyoua === "contractor") {
-       $profileImageName = null; 
-
-    if ($request->hasFile('profile_image')) {
-        $profileImage = $request->file('profile_image');
-        $profileImageName = time() . '_' . $profileImage->getClientOriginalName();
-        $profileImage->storeAs('uploads/contractor_profile', $profileImageName);
-    }
-    $portfolioPaths = [];
-    
-    if ($request->hasFile('uploadimagerecentwork')) {
-        foreach ($request->file('uploadimagerecentwork') as $portfolioImage) {
-            $portfolioImageName = time() . '_' . $portfolioImage->getClientOriginalName();
-            $portfolioImage->storeAs('uploads/contractor_portfolio', $portfolioImageName);
-            $portfolioPaths[] = 'uploads/contractor_portfolio/' . $portfolioImageName;
+        $portfolioPaths = [];
+        
+        if ($request->hasFile('uploadimagerecentwork')) {
+            foreach ($request->file('uploadimagerecentwork') as $portfolioImage) {
+                $portfolioImageName = time() . '_' . $portfolioImage->getClientOriginalName();
+                $portfolioImage->storeAs('uploads/contractor_portfolio', $portfolioImageName);
+                $portfolioPaths[] = 'uploads/contractor_portfolio/' . $portfolioImageName;
+            }
         }
+        
+        $contractor                        = new Contractor;
+        $contractor->name                  = $request->name;
+        $contractor->email                 = $request->email;
+        $contractor->company_name          = $request->companyname ?? '';
+        $contractor->password              = bcrypt($request->password);
+        $contractor->contact_number        = $request->contact_number;
+        $contractor->zip_code              = $request->zip_code;
+        $contractor->profile_image         = $profileImageName ? 'uploads/contractor_profile/' . $profileImageName : null;
+        $contractor->contractor_portfolio  = $portfolioPaths;
+        $contractor->save();
+        }
+        
+        return view("layouts.front.Auth.register-thankyou");
     }
-    
-    $contractor                        = new Contractor;
-    $contractor->name                  = $request->name;
-    $contractor->email                 = $request->email;
-    $contractor->company_name          = $request->companyname ?? '';
-    $contractor->password              = bcrypt($request->password);
-    $contractor->contact_number        = $request->contact_number;
-    $contractor->zip_code              = $request->zip_code;
-    $contractor->profile_image         = $profileImageName ? 'uploads/contractor_profile/' . $profileImageName : null;
-    $contractor->contractor_portfolio  = $portfolioPaths;
-    $contractor->save();
-    }
-    
-    return view("layouts.front.Auth.register-thankyou");
-}
 
-
+    
 }
